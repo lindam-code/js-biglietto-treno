9fe3ca9816716432f54c9540cdc0ee1f19379ed8
lindam <linda.minotti2@gmail.com>
2020-05-20 15:58:58 +0200

    Validazione input e calcolo biglietto con sconti per età.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 // Chiedi all'utente quanti chilometri debve percorrere con il treno.
 var km = parseInt(prompt('Scrivi in numero quanti km devi percorrere con il treno.'));
-console.log(km);
 
 // Tariffa a chilometro.
 var tariffa = 0.21;
 
 //Calcolo del prezzo totale senza eventuali sconti.
 var prezzoTotale = tariffa * km;
-console.log(prezzoTotale);
 
 // Validazione e output
 if (!isNaN(prezzoTotale)) {
@@ -21,11 +19,17 @@ var eta = parseInt(prompt('Scrivi quanti anni hai, in numero.'))
 console.log(eta);
 
 // Applicazione sconti in base all'età
+var scontoMinorenni = prezzoTotale * 0.2;
+var scontoOver65 = prezzoTotale * 0.4;
+var prezzoScontatoMinorenni = prezzoTotale - scontoMinorenni;
+var prezzoScontatoOver65 = prezzoTotale - scontoOver65;
+
+// Output prezzo scontato in base all'età
 if (!isNaN(eta)) {
   if (eta < 18) {
-    document.getElementById('prezzo_scontato').innerHTML = 'Prezzo per minorenni.';
+    document.getElementById('prezzo_scontato').innerHTML = 'Il prezzo del biglietto scontato è ' + prezzoScontatoMinorenni;
   } else if (eta > 65) {
-    document.getElementById('prezzo_scontato').innerHTML = 'Prezzo over 65.';
+    document.getElementById('prezzo_scontato').innerHTML = 'Il prezzo del biglietto scontato è ' + prezzoScontatoOver65;
   } else {
     document.getElementById('prezzo_scontato').innerHTML = 'Non ci sono sconti a te dedicati.';
   }
