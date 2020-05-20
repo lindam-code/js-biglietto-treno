b2ba5c8d2ba4ed8c487c6fae4c1946d019b3789a
lindam <linda.minotti2@gmail.com>
2020-05-20 15:45:47 +0200

    Validazioni e condizioni per età.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Chiedi all'utente quanti chilometri debve percorrere con il treno.
 var km = parseInt(prompt('Scrivi in numero quanti km devi percorrere con il treno.'));
-
 console.log(km);
+
 // Tariffa a chilometro.
 var tariffa = 0.21;
 
@@ -12,4 +12,23 @@ console.log(prezzoTotale);
 // Validazione e output
 if (!isNaN(prezzoTotale)) {
   document.getElementById('prezzo_biglietto').innerHTML = 'Il prezzo del biglietto è di ' + prezzoTotale + ' €.'
+} else {
+  alert('Errore: Non è un numero!!')
+}
+
+// Chiedi all'utente la sua età per applicare eventuali sconti
+var eta = parseInt(prompt('Scrivi quanti anni hai, in numero.'))
+console.log(eta);
+
+// Applicazione sconti in base all'età
+if (!isNaN(eta)) {
+  if (eta < 18) {
+    document.getElementById('prezzo_scontato').innerHTML = 'Prezzo per minorenni.';
+  } else if (eta > 65) {
+    document.getElementById('prezzo_scontato').innerHTML = 'Prezzo over 65.';
+  } else {
+    document.getElementById('prezzo_scontato').innerHTML = 'Non ci sono sconti a te dedicati.';
+  }
+} else {
+  alert('Errore: Non è un numero!!')
 }
