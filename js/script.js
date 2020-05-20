207656d33661fdd48b85de15e1dee3d43d6c2c9c
lindam <linda.minotti2@gmail.com>
2020-05-20 16:41:18 +0200

    Codice più organizzato.

--- js/script.js
+++ js/script.js
@@ -1,38 +1,35 @@
-// Chiedi all'utente quanti chilometri debve percorrere con il treno.
+// Chiedi all'utente quanti chilometri deve percorrere con il treno quanti anni ha per applicare sconti eventuali.
 var km = parseInt(prompt('Scrivi in numero quanti km devi percorrere con il treno.'));
+var eta = parseInt(prompt('Scrivi quanti anni hai, in numero.'))
 
 // Tariffa a chilometro.
 var tariffa = 0.21;
 
-//Calcolo del prezzo totale senza eventuali sconti.
+//Calcolo del prezzo totale e sconti.
 var prezzoTotale = tariffa * km;
+var scontoMinorenni = prezzoTotale * 0.2;
+var scontoOver65 = prezzoTotale * 0.4;
+
+// Calcolo prezzi scontati
+var prezzoScontatoMinorenni = prezzoTotale - scontoMinorenni;
+var prezzoScontatoOver65 = prezzoTotale - scontoOver65;
 
 // Validazione e output
 if (!isNaN(prezzoTotale)) {
   document.getElementById('prezzo_biglietto').innerHTML = 'Il prezzo del biglietto è di ' + prezzoTotale + ' €.'
 } else {
-  alert('Errore: Non è un numero!!')
+  alert('Non posso calcolare il prezzo totale perchè non mi hai dato il numero dei kilometri!!')
 }
-
-// Chiedi all'utente la sua età per applicare eventuali sconti
-var eta = parseInt(prompt('Scrivi quanti anni hai, in numero.'))
-console.log(eta);
-
-// Applicazione sconti in base all'età
-var scontoMinorenni = prezzoTotale * 0.2;
-var scontoOver65 = prezzoTotale * 0.4;
-var prezzoScontatoMinorenni = prezzoTotale - scontoMinorenni;
-var prezzoScontatoOver65 = prezzoTotale - scontoOver65;
 
 // Output prezzo scontato in base all'età
 if (!isNaN(eta)) {
   if (eta < 18) {
-    document.getElementById('prezzo_scontato').innerHTML = 'Il prezzo del biglietto scontato è ' + prezzoScontatoMinorenni;
+    document.getElementById('prezzo_scontato').innerHTML = 'Il prezzo del biglietto scontato è ' + prezzoScontatoMinorenni + ' €.';
   } else if (eta > 65) {
-    document.getElementById('prezzo_scontato').innerHTML = 'Il prezzo del biglietto scontato è ' + prezzoScontatoOver65;
+    document.getElementById('prezzo_scontato').innerHTML = 'Il prezzo del biglietto scontato è ' + prezzoScontatoOver65 + ' €.';
   } else {
     document.getElementById('prezzo_scontato').innerHTML = 'Non ci sono sconti a te dedicati.';
   }
 } else {
-  alert('Errore: Non è un numero!!')
+  alert("Non posso calcolare gli sconti perchè l'età che mi hai dato non è un numero valido!!")
 }
